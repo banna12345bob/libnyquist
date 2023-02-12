71eea3676dceab70b25d6ae54a40df5dbf89e6b6
banna12345bob <banna12345bob@outlook.com>
2023-02-12 19:29:36 +1000

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,54 @@
+project "lubnyquist"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/*.c",
+		"src/*.cpp",
+	}
+
+	includedirs
+	{
+		"include/libnyquist",
+		"third_party",
+		"third_party/FLAC/src/include",
+		"third_party/libogg/include",
+		"third_party/libvorbis",
+		"third_party/musepack/include",
+		"third_party/opus",
+		"third_party/opus/libopus/include",
+		"third_party/opus/celt",
+		"third_party/libvorbis/include",
+		"third_party/opus/silk/float",
+		"third_party/opus/silk",
+		"third_party/opus/opusfile/src/include",
+		"third_party/opus/opusfile/include",
+		"third_party/wavpack/include"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++11"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++11"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
